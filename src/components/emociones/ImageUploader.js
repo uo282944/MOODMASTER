356dbc79329e5621508998de28385356e42add1b
Javier <uo282944@uniovi.es>
2024-01-05 05:01:02 +0100

    Añadido correctamente el modelo

--- src/components/emociones/ImageUploader.js
+++ src/components/emociones/ImageUploader.js
@@ -13,32 +13,49 @@ const ImageUploader = () => {
     };
   
     async function runModel() {
-      const model = await tf.loadGraphModel('https://raw.githubusercontent.com/daved01/tensorflowjs-web-app-demo/main/models/fullyConvolutionalModelTfjs/model.json');
-  
-      // Get content image
-      let image = new Image(256,256);
-      image.src = exampleImage;
-  
-      // Convert image to tensor and add batch dimension
-      let tfTensor = tf.browser.fromPixels(image);    
-      tfTensor = tfTensor.div(255.0);
-      tfTensor = tfTensor.expandDims(0);
-      tfTensor = tfTensor.cast("float32");
-      
-      // Run image through model
-      const pred = model.predict(tfTensor);
-      
-      // Convert tensor to image
-      let outputTensor = pred.squeeze();
-      
-      // Scale to range [0,1] from [-1,1]
-      outputTensor = outputTensor.mul(0.5);
-      outputTensor = outputTensor.add(0.5);
-  
-      // Prepare rendering of the result
-      const canvas = document.getElementById('canvas'); 
-      await tf.browser.toPixels(outputTensor, canvas);      
-  };
+      const model = await tf.loadLayersModel('/modelo/model.json');
+
+      // Asume que 'image' es un objeto File obtenido del input
+      const imageElement = document.createElement('img');
+      const imageUrl = URL.createObjectURL(image);
+      imageElement.src = imageUrl;
+
+      try {
+        await new Promise((resolve, reject) => {
+          imageElement.onload = resolve;
+          imageElement.onerror = reject;
+        });
+
+        const tensorImagen = tf.browser.fromPixels(imageElement)
+          .resizeNearestNeighbor([48, 48])
+          .mean(2)
+          .expandDims(2)
+          .expandDims()
+          .toFloat()
+          .div(255.0);
+
+        // Ejecutar el modelo con la imagen
+        const predicciones = model.predict(tensorImagen);
+
+        // Etiquetas de las emociones
+        const etiquetas = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised'];
+
+        // Mostrar las predicciones con etiquetas en la consola
+        predicciones.data().then(data => {
+          const resultados = Array.from(data).map((prob, index) => {
+            return { etiqueta: etiquetas[index], probabilidad: prob };
+          });
+          console.log("Resultados de la predicción con etiquetas:", resultados);
+        });
+
+      } catch (error) {
+        console.error(error);
+      } finally {
+        // Limpieza
+        URL.revokeObjectURL(imageUrl);
+        imageElement.remove();
+      }
+    }
   
     return (
       <div className="imguploader-container">
